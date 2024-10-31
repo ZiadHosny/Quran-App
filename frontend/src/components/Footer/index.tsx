704e32454e6149dc9b1e750e12da570225f9d6e9
Ziad Hosny <ziadhosny007@gmail.com>
2024-10-31 15:19:24 +0300

    add Privacy Policy to app

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,15 +1,16 @@
-import { VERSION } from '../../utils/envs'
-import './footer.scss'
+import { PrivacyPolicy, VERSION } from "../../utils/envs";
+import "./footer.scss";
 
 export const Footer = () => {
-
-    return (
-        <footer className='flex-center-center'>
-            <a className='link' href="https://ziadhosny.vercel.app/">
-                Ziad Hosny
-            </a>
-            |
-            <p>Version : {VERSION}</p>
-        </footer>
-    )
-}
+  return (
+    <footer className="flex-center-center">
+      <a className="link" href="https://ziadhosny.vercel.app/">
+        Ziad Hosny
+      </a>
+      |<p>Version : {VERSION}</p>|
+      <a className="link" rel="noreferrer" target="_blank" href={PrivacyPolicy}>
+        Privacy Policy
+      </a>
+    </footer>
+  );
+};
--- frontend/src/utils/envs.ts
+++ frontend/src/utils/envs.ts
@@ -1,5 +1,6 @@
-export const backendUrl = process.env.REACT_APP_BACKEND_URL
-export const domain = process.env.REACT_APP_DOMAIN || ''
-export const clientId = process.env.REACT_APP_CLIENT_ID || ''
-export const publicKey = process.env.REACT_APP_PUBLIC_KEY || ''
-export const VERSION = "3.0.1"
+export const backendUrl = process.env.REACT_APP_BACKEND_URL;
+export const domain = process.env.REACT_APP_DOMAIN || "";
+export const clientId = process.env.REACT_APP_CLIENT_ID || "";
+export const publicKey = process.env.REACT_APP_PUBLIC_KEY || "";
+export const VERSION = "4.0.0";
+export const PrivacyPolicy = "https://sites.google.com/view/quran-app-z";
