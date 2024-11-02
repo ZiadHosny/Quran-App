414a10dfc39af74db59485af4d2f59f3063bb936
Ziad Hosny <ziadhosny007@gmail.com>
2024-11-02 21:30:30 +0200

    try fix ios

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,40 +1,43 @@
-import { useEffect } from 'react'
-import { QuranReciter } from '../components/QuranReciter'
-import { useProgress } from '../hooks/useProgress';
-import { useSurah } from '../hooks/useSurah';
+import { useEffect } from "react";
+import { QuranReciter } from "../components/QuranReciter";
+import { useProgress } from "../hooks/useProgress";
+import { useSurah } from "../hooks/useSurah";
 
 export const Home = () => {
   const { getAllQuranReciters } = useProgress();
   const { quranRecitersFilter } = useSurah();
 
   useEffect(() => {
-    if (Notification.permission !== "granted") {
+    if (Notification && Notification.permission !== "granted") {
       Notification.requestPermission().then(function (permission) {
-        console.log('permission : ', permission)
+        console.log("permission : ", permission);
       });
     }
     const fn = async () => {
-      await getAllQuranReciters()
-    }
-    fn()
+      await getAllQuranReciters();
+    };
+    fn();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   return (
     <main>
-      <div className='container'>
+      <div className="container">
         <QuranReciter
-          key={'mostPlayed'}
-          bgColor={'#ffd52d'}
+          key={"mostPlayed"}
+          bgColor={"#ffd52d"}
           imgSize={100}
           quranReciter={{
-            id: 'mostPlayed', quranReciter: 'الأكثر تشغيلا',
-            photo: 'https://images.unsplash.com/photo-1609599006353-e629aaabfeae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          }} />
+            id: "mostPlayed",
+            quranReciter: "الأكثر تشغيلا",
+            photo:
+              "https://images.unsplash.com/photo-1609599006353-e629aaabfeae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          }}
+        />
         {quranRecitersFilter.map((quranReciter) => (
           <QuranReciter key={quranReciter.id} quranReciter={quranReciter} />
         ))}
       </div>
     </main>
-  )
-}
+  );
+};
