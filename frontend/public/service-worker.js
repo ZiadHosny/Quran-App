f396a4bf03ff00898c6cb67b70044c25cd4363bb
Ziad Hosny <ziadhosny007@gmail.com>
2024-11-02 21:05:13 +0200

    try to fix ios

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,7 +1,19 @@
 /* eslint-disable no-restricted-globals */
-self.addEventListener('push', () => {
-    self.registration.showNotification('Quran App', {
-        body: "Notified by Quran!",
-        icon: "images/quran128.png"
-    })
-})
+self.addEventListener("push", () => {
+  self.registration.showNotification("Quran App", {
+    body: "Notified by Quran!",
+    icon: "images/quran128.png",
+  });
+});
+
+// self.addEventListener('push', event => {
+//     const notificationOptions = {
+//         body: "Notified by Quran!",
+//         icon: "images/quran128.png",
+//         tag: "quran-notification"  // Optional: Prevents stacking of similar notifications
+//     };
+
+//     event.waitUntil(
+//         self.registration.showNotification('Quran App', notificationOptions)
+//     );
+// });
